ef34624b691dd35cd7ac34691b091091281f3aa2
Christian <Chrisaduenas@gmail.com>
2020-07-16 17:18:14 -0700

    lru_cache decorator added to eating cookies algorithm

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,6 +2,9 @@
 Input: an integer
 Returns: an integer
 '''
+import functools
+
+@functools.lru_cache()
 def eating_cookies(n):
     # Your code here
     #Base cases
@@ -22,8 +25,9 @@ def eating_cookies(n):
     #Use recursion to simulate 1 , 2 , or 3 at a time and combine the combinations
     return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
 
+
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
 
-    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
+    print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
